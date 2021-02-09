ff37d975daf40378724c12e6035d9ab6cca8ccb9
unknown <bart@bniemczewski.pl>
2021-02-09 01:58:30 +0100

    Add preview page with auto refresh

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 #  """
 #  -----------------------------------------------------------------------
-#  Designed and implemented by BartN
+#  Designed and implemented by bartiniem
 #  -----------------------------------------------------------------------
 #  """
 
 # IMPORTS
 import os
 from datetime import datetime
-
-import yaml
 from flask import Flask, session, redirect, url_for, request, render_template
 
-# DEFINITIONS
 from datautils import DataUtils
 
+# DEFINITIONS
 CARDS_FILE = "data/cards.yaml"
 VOTES_FILE = "data/votes.yaml"
 
@@ -27,14 +25,24 @@ app.config['next_id'] = 100
 @app.route('/', methods=['GET', 'POST'])
 def index():
     last_update = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # gen_cards = DataUtils().get_cards()
-    # DataUtils().save_data_to_yaml(gen_cards, CARDS_FILE)
     cards = DataUtils().get_cards(CARDS_FILE)
     good_cards = [card for card in cards if card.get("type") in "good"]
     bad_cards = [card for card in cards if card.get("type") in "bad"]
     votes = DataUtils().load_yaml_data(VOTES_FILE)
     sum_cards = len(cards)
     return render_template('dashboard.html', title="BoardViewer", good_cards=good_cards, bad_cards=bad_cards,
+                           last_update=last_update, votes=votes, sum_cards=sum_cards)
+
+
+@app.route('/preview', methods=['GET', 'POST'])
+def preview():
+    last_update = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cards = DataUtils().get_cards(CARDS_FILE)
+    good_cards = [card for card in cards if card.get("type") in "good"]
+    bad_cards = [card for card in cards if card.get("type") in "bad"]
+    votes = DataUtils().load_yaml_data(VOTES_FILE)
+    sum_cards = len(cards)
+    return render_template('preview.html', title="BoardViewer", good_cards=good_cards, bad_cards=bad_cards,
                            last_update=last_update, votes=votes, sum_cards=sum_cards)
 
 
@@ -50,8 +58,8 @@ def add_card():
             card_text = request.form.get("card_text") if request.form.get("card_text") else ""
             card_type = request.form.get("card_type") if request.form.get("card_type") else ""
             cards = DataUtils().get_cards(CARDS_FILE)
-            new_card = {"author": card_author, "emotions": card_emotions, "id": id_to_set, "name": card_text, "points": "",
-                        "show": False, "type": card_type}
+            new_card = {"author": card_author, "emotions": card_emotions, "id": id_to_set, "name": card_text,
+                        "points": "", "show": False, "type": card_type}
             cards.append(new_card)
             DataUtils().save_data_to_yaml(cards, CARDS_FILE)
             message = "New card added: {}".format(new_card)
