af21b03fa26a89bad8570a73110bef23d1789fa9
unknown <bart@bniemczewski.pl>
2021-02-09 01:38:53 +0100

    Update form for adding cards

--- datautils.py
+++ datautils.py
@@ -9,7 +9,7 @@ class DataUtils:
     @staticmethod
     def save_data_to_yaml(my_data, dict_file):
         with open(dict_file, 'w') as file:
-            yaml.dump(my_data, file)
+            yaml.dump(my_data, file, default_flow_style=False)
 
     @staticmethod
     def load_yaml_data(dict_file):
